8cafd6ac55b0e3c9366dc0355131a1ecac91ce7b
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-08-23 16:20:04 +0300

    Update main.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_init():
+    item3 = Item("TV", 15000, 2)
+    assert item3.name == "TV"
+    assert item3.price == 15000
+    assert item3.quantity == 2
+
+def test_calculate_total_price():
+    item = Item("Смартфон", 10000, 20)
+    assert item.calculate_total_price() == 200000
+
+def test_apply_discount():
+    item1 = Item("TV", 10000, 2)
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
+
+# test_init()
+# test_calculate_total_price()
+# test_apply_discount()
+
+
