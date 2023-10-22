c92f2d835e9e4b94d8111f759e1392dcf2e456d8
Mary Melnikova <marymelnikova1988@gmail.com>
2023-10-22 20:44:19 +0300

    Update main.py (hw 4)

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -11,9 +11,11 @@ def test_init():
 def test_repr():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+
 def test_str():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert str(phone1) == 'iPhone 14'
+
 def test_number_of_sim():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert phone1.number_of_sim == 2
